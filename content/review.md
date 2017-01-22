704a0e25fa4a66e25cd92aeb758c2d614d2e9260
Tomke Reibisch <treibisch@gmail.com>
2017-01-22 14:30:24 +0100

    Initial

---
+++ content/review.md
@@ -0,0 +1,5 @@
+Title: Here might be stuff soon!
+Date: 2017-01-22 14:09
+Category: Setup
+
+Following is nothing yet.
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Tomke Reibisch'
+SITENAME = u'Doing Stuff'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Gregor Codes', 'http://gregor.codes'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/reibisch'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
